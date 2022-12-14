26887e2145aa2f54c9301379dc795c3b93c09c9e
shoorik007 <31213414+shoorik007@users.noreply.github.com>
2022-12-14 21:16:38 +0200

    Create Ukrainian.ts

---
+++ translation/Ukrainian.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>CAboutWnd</name>
+    <message>
+        <source>•</source>
+        <translation>•</translation>
+    </message>
+    <message>
+        <source>Corey</source>
+        <translation>Corey</translation>
+    </message>
+    <message>
+        <source>About Author</source>
+        <translation>Про автора</translation>
+    </message>
+    <message>
+        <source>Send feedback : )</source>
+        <translation>Є ідея? Напишіть мені :)</translation>
+    </message>
+    <message>
+        <source>Gratitude List</source>
+        <translation>Дякую</translation>
+    </message>
+    <message>
+        <source>1218.io</source>
+        <translation>1218.io</translation>
+    </message>
+    <message>
+        <source>Translator</source>
+        <translation>Переклад</translation>
+    </message>
+    <message>
+        <source>cc.seer@gmail.com</source>
+        <translation>cc.seer@gmail.com</translation>
+    </message>
+    <message>
+        <source>About Seer</source>
+        <translation>Про програму Seer</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Версія</translation>
+    </message>
+    <message>
+        <source>A Windows Quick Look Tool</source>
+        <translation>Інструмент швидкого перегляду</translation>
+    </message>
+    <message>
+        <source>Cisa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>FOSS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Source Software in Seer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
+
